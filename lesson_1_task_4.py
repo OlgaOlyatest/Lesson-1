aace0487368345e6a6add8cac4ef8c7569d5fda1
OlgaOlyatest <154988895+OlgaOlyatest@users.noreply.github.com>
2024-01-17 16:21:22 +0500

    Add files via upload

---
+++ lesson_1_task_4.py
@@ -0,0 +1,3 @@
+first_name = input("Olga")
+last_name = input("Medvedeva")
+print("Вас зовут:" + first_name + last_name)
---
+++ lesson_1_task_6.py
@@ -0,0 +1,5 @@
+greeting = ("Привет, мир!")
+def print_greeting():
+    print(greeting)
+
+print_greeting()
